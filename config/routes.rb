6369b30b891ab22b2b810249ad4f96561bed7bc9
Nana Baah Akuffu <nakuffu@aims.ac.tz>
2020-01-20 11:01:06 +0000

    Added the signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Routes for home and about pages
   root "pages#home"
   get "about", to: "pages#about"
 
+  # Routs for articles
   resources :articles
+
+  # Routes for user signup
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
