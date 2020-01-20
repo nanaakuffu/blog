b1437863f4e2f5b439e5fbb88a017584232d0c32
Nana Baah Akuffu <nakuffu@aims.ac.tz>
2020-01-20 11:48:42 +0000

    created add and edit users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_params, only: [:edit, :update, :destroy, :show]
     def new
         @user = User.new
     end
@@ -6,14 +7,38 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "Welcome to the blog #{user.username}"
+            flash[:success] = "Welcome to the blog #{@user.username}"
             redirect_to articles_path
         else
             render "new"
         end
     end
 
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if user.update(user_params)
+            flash[:success] = "Account updated successfully"
+            redirect_to user_path(@user)
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        @user.destroy
+        flash[:danger] = "User was successfully deleted"
+        redirect_to users_path
+    end
+
     private
+        def set_params
+            @user = User.find(params[:id])
+        end
         def user_params
             params.require(:user).permit(:username, :email, :password)
         end
