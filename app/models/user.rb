59fb9be36a2a225b01e0be7a647273918b7e3710
Nana Baah Akuffu <nakuffu@aims.ac.tz>
2020-01-20 09:42:42 +0000

    Created relationship between article and user table. Added degugger to application template.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+    has_many :articles
+    before_save { self.email = email.downcase }
+    
+    # Validations for username field for data-input
     validates :username, presence: true, 
                uniqueness: { case_sensitive: false }, 
                length: { minimum: 3, maximum: 25 }
+
+    # Validations for email field for data-input
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 105 },
                uniqueness: { case_sensitive: false },
