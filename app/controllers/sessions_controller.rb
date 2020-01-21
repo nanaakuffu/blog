ae2150caeac39bae47a8722acac80b4f3f50b2e9
Nana Baah Akuffu <nakuffu@aims.ac.tz>
2020-01-21 14:13:22 +0000

    Worked on the new, create and destroy mwthods for login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,26 +4,21 @@ class SessionsController < ApplicationController
     end
     
     def create
-        # @object = Object.new(params[:object])
-        # if @object.save
-        #   flash[:success] = "Object successfully created"
-        #   redirect_to @object
-        # else
-        #   flash[:error] = "Something went wrong"
-        #   render 'new'
-        # end
-        render "new"
+        user = User.find_by(email: params[:sessions][:email])
+        if user && user.authenticate(params[:sessions][:password])
+            session[:user_id] = user.id
+            flash[:succes] = "Welcome to your page #{user}!"
+            redirect_to user_url(user)
+        else
+            flash.now[:danger] = "Incorrect password or username"
+            render "new"
+        end
     end
     
     def destroy
-        @object = Object.find(params[:id])
-        if @object.destroy
-            flash[:success] = 'Object was successfully deleted.'
-            redirect_to objects_url
-        else
-            flash[:error] = 'Something went wrong'
-            redirect_to objects_url
-        end
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out. Come back soon!"
+        redirect_to root_url
     end
     
 end
